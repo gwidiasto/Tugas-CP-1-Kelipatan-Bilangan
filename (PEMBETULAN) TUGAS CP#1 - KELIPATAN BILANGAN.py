5abac993c7086678c2c5410fe8424541f76110d9
Gama Widiasto <>
2017-11-14 23:16:26 +0700

    PEMBETULAN CP 1
    Gama Widiasto
    04217017

---
+++ (PEMBETULAN) TUGAS CP#1 - KELIPATAN BILANGAN.py
@@ -0,0 +1,10 @@
+print("TUGAS CP#1 - KELIPATAN BILANGAN")
+
+a=int (input ("Masukan Angka Awal = "))
+i=int (input ("Masukan Kelipatan = "))
+u=int (input ("Masukan Perulangan = "))
+
+print ("Hasil :")
+for s in range (u):
+        print (a)
+        a=a+i
